75f5ae0c386aa600f3256dff588abf4cbb0201b8
Kevin Sjöberg <kev.sjoberg@gmail.com>
2018-02-09 14:30:57 +0100

    Add ability to cache data providers

--- lib/data_provider/test/data_provider_test.rb
+++ lib/data_provider/test/data_provider_test.rb
@@ -2,10 +2,18 @@ require 'minitest/autorun'
 require_relative '../lib/data_provider'
 
 class GitHubProvider
+  DATA = [{ data: 2, type: 'number', title: 'Antal' }].freeze
+  EXPIRES_IN_SECONDS = 60
+  Response = Struct.new(:data, :expires_at)
+
   attr_reader :options
 
   def initialize(options = {})
     @options = options
+  end
+
+  def fetch
+    Response.new(DATA, Time.now + EXPIRES_IN_SECONDS)
   end
 end
 
@@ -25,4 +33,48 @@ class DataProviderTest < Minitest::Test
     assert_instance_of DataProvider::Provider, DataProvider.providers['github']
   end
 
+  def test_fetch_empty_cache
+    cache_store = Minitest::Mock.new
+
+    DataProvider.configure do |c|
+      c.cache_store = cache_store
+    end
+
+    DataProvider.register :github, GitHubProvider
+
+    cache_store.expect :fetch, nil, [
+      'github/data'
+    ]
+
+    cache_store.expect :write, true, [
+      'github/data',
+      GitHubProvider::DATA,
+      expires_in: GitHubProvider::EXPIRES_IN_SECONDS
+    ]
+
+    Timecop.freeze(Time.now) do
+      DataProvider.providers[:github].fetch
+      assert_mock cache_store
+    end
+  end
+
+  def test_fetch_with_data_in_cache
+    cache_store = Minitest::Mock.new
+
+    DataProvider.configure do |c|
+      c.cache_store = cache_store
+    end
+
+    DataProvider.register :github, GitHubProvider
+
+    cache_store.expect :fetch, GitHubProvider::DATA, [
+      'github/data'
+    ]
+
+    Timecop.freeze(Time.now) do
+      data = DataProvider.providers[:github].fetch
+      assert_mock cache_store
+      assert_equal GitHubProvider::DATA, data
+    end
+  end
 end
