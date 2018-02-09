864ede3d6ae988af1343caa24815e0127ec0e5f3
Kevin Sjöberg <kev.sjoberg@gmail.com>
2018-02-09 15:51:35 +0100

    Remove controller test

--- test/controllers/dashboards_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class DashboardsControllerTest < ActionDispatch::IntegrationTest
-  test 'render show' do
-    get dashboard_path('provider')
-    assert_response :success
-  end
-end
