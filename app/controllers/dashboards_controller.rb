2994028191633978402d6cae2bc425cc3eb59f31
Victor Wallin Pelz <victor.wallin@standout.se>
2018-02-09 16:40:01 +0100

    Add first styling change

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -29,7 +29,7 @@ class DashboardsController < ApplicationController
           headers: ['Namn', 'Antal PRs', 'Användarnamn']
         },
         data: [
-          ['David', 999, 'davidelbe'],
+          ['David', 8, 'davidelbe'],
           ['Emric', 2, 'istanful'],
           ['Tobias', 2, 'sandelius'],
           ['Ross', 2, 'alex-ross'],
